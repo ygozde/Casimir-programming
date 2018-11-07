8222f32e652a70f40eb655880b858021326826b4
Lui-G <l.a.maduro@tudelft.nl>
2018-11-07 15:21:55 +0100

    just some prerequisites

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 import numpy as np
 import matplotlib as plt
+import scipy
+
+
+
 
 print('Hello World!')
+
+
+#### Need to load raw data and save it into numpy array
+#### Need to generate a file with raw data, e.g. .txt file
+
+
+
