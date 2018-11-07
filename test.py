1ad26fce129d76a0737333f8bfb6035feda7137d
ygozde <y.g.saglam@tudelft.nl>
2018-11-07 16:59:09 +0100

    commit message

--- test.py
+++ test.py
@@ -1,1 +1,30 @@
-print('Hello World!')
+print('This is a trial!')
+
+#Version 2018-11-07 10:40
+#------------------------------
+
+
+import numpy as np
+print("Hello World!")
+
+
+#Version 2018-11-07 11:14
+# Made the function circumz(r_1) for circumference
+
+import numpy as np
+#print("Hello World!")
+
+r = 5
+circum = 2* np.pi* r
+print(circum)
+
+def circumz(r_1):
+        return  2 * np.pi * r_1
+print(circumz(3))
+
+area = np.pi* r* r
+##print(area)
+
+def area(r_2):
+    return np.pi* r* r
+print(area(4))
